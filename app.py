58367b1c9495b2f5269122595861d6499b2f4913
Ladonean <dominik01pitula@gmail.com>
2025-03-14 15:09:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ if map_data.get("last_clicked"):
     clicked_lat = map_data["last_clicked"]["lat"]
     clicked_lng = map_data["last_clicked"]["lng"]
     st.session_state.map_center = [clicked_lat, clicked_lng]
-    st.session_state.zoom = 20
+    st.session_state.zoom = 100
 
 # Rozpoczęcie licznika – ustawiamy start_time przy pierwszym dodaniu węzła
 if st.session_state.route and st.session_state.start_time is None:
